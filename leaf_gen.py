d175cb1b46c15960a94aaa0eda1b4cc8eba3eb98
Jacob Green <jacobgreen@Jacobs-MacBook-Air.local>
2024-10-20 20:09:37 +0100

    Add small veins

--- leaf_gen.py
+++ leaf_gen.py
@@ -7,30 +7,98 @@ init_roundness = 1
 init_spikes = 0
 init_sa = 0.1
 init_w = 1.0
+init_vein_length = 0.8
 
-l = 1.0
+vein_steps = 10
+
+t = np.linspace(0, np.pi, 1000)
 
 def normalize(x):
     return (x - np.min(x)) / (np.max(x) - np.min(x))
 
-def get_leaf_side(roundness, l, s, sa, w):
-    t = np.linspace(0, np.pi, 1000)
-
+def get_r(t, roundness, s, sa):
+    l = 1.0
     r = l / (np.abs(np.cos(t / 4)) + np.abs(np.sin(t / 4))) ** (1 / roundness)
     r = r + sa * (np.sin(s * t) - 0.5 * np.cos(7 * t))
 
-    y = np.cos(t) * r
+    return normalize(r)
+
+def get_leaf_side(roundness, s, sa, w):
+    r = get_r(t, roundness, s, sa)
+
     x = np.sin(t) * r
-    return normalize(x) * w, normalize(y)
+    y = np.cos(t) * r
+
+    return x * w, y
+
+def get_main_vein(vl):
+    x = np.zeros(vein_steps)
+    y = np.linspace(0, vl, vein_steps)
+
+    return x, y
+
+def update(val):
+    x, y = get_leaf_side(roundness_slider.val, spikes_slider.val, sa_slider.val, width_slider.val)
+    line.set_xdata(x)
+    line.set_ydata(y)
+    line2.set_xdata(-x)
+    line2.set_ydata(y)
+    fig.canvas.draw()
+    draw_small_veins()
+    update_vein()
+
+small_veins = []
+
+def clear_small_veins():
+    for sv in small_veins:
+        sv.remove()
+    
+    small_veins.clear()
+
+def get_side_vein(vein_y):
+    all_points_x, all_points_y = get_leaf_side(roundness_slider.val, spikes_slider.val, sa_slider.val, width_slider.val)
+    closest_point_idx = np.argmin(np.abs(all_points_y - vein_y))
+    vein_x = 0.8 * all_points_x[closest_point_idx]
+
+    x = np.linspace(0, vein_x, vein_steps)
+    y = vein_y + 0.02 * np.sin(np.cos(vein_y * 7) + x * np.pi * 4)
+    return x, y
+
+def draw_small_vein(vein_y):
+    x, y = get_side_vein(vein_y)
+    small_veins.append(ax.plot(x, y, lw=1, color='brown')[0])
+    small_veins.append(ax.plot(-x, y, lw=1, color='brown')[0])
+
+def draw_small_veins():
+    clear_small_veins()
+
+    num_small_veins = num_veins_slider.val
+
+    for i in range(num_small_veins):
+        vein_y = i * main_vein_slider.val / num_small_veins
+        print(vein_y)
+        draw_small_vein(vein_y)
+
+    fig.canvas.draw()
+
+def update_vein():
+    _, y = get_main_vein(main_vein_slider.val)
+    main_vein_line.set_ydata(y)
+    draw_small_veins()
+    fig.canvas.draw()
+
 
 
 # Create the figure and the line that we will manipulate
 fig, ax = plt.subplots()
 
-x, y = get_leaf_side(init_roundness, l, init_spikes, init_sa, init_w)
+x, y = get_leaf_side(init_roundness, init_spikes, init_sa, init_w)
 
 line, = ax.plot(x, y, lw=1, color='g')
 line2, = ax.plot(-x, y, lw=1, color='g')
+
+x, y = get_main_vein(init_vein_length)
+main_vein_line, = ax.plot(x, y, lw=1, color='brown')
 
 # adjust the main plot to make room for the sliders
 fig.subplots_adjust(left=0.5, bottom=0.25)
@@ -39,6 +107,9 @@ ax_roundness = fig.add_axes([0.1, 0.25, 0.0225, 0.63])
 ax_spikes = fig.add_axes([0.2, 0.25, 0.0225, 0.63])
 ax_sa = fig.add_axes([0.3, 0.25, 0.0225, 0.63])
 ax_w = fig.add_axes([0.4, 0.25, 0.0225, 0.63])
+
+ax_mv = fig.add_axes([0.2, 0.1, 0.63, 0.0225])
+ax_nv = fig.add_axes([0.2, 0.15, 0.63, 0.0225])
 
 roundness_slider = Slider(
     ax=ax_roundness,
@@ -80,18 +151,32 @@ width_slider = Slider(
     orientation="vertical"
 )
 
-def update(val):
-    x, y = get_leaf_side(roundness_slider.val, l, spikes_slider.val, sa_slider.val, width_slider.val)
-    line.set_xdata(x)
-    line.set_ydata(y)
-    line2.set_xdata(-x)
-    line2.set_ydata(y)
-    fig.canvas.draw()
+main_vein_slider = Slider(
+    ax=ax_mv,
+    label="main vein",
+    valmin=0.1,
+    valmax=1.0,
+    valinit=init_vein_length,
+    valstep=0.01,
+    orientation="horizontal"
+)
 
+num_veins_slider = Slider(
+    ax=ax_nv,
+    label="num veins",
+    valmin=1,
+    valmax=50,
+    valinit=init_vein_length,
+    valstep=1,
+    orientation="horizontal"
+)
 
 roundness_slider.on_changed(update)
 spikes_slider.on_changed(update)
 sa_slider.on_changed(update)
 width_slider.on_changed(update)
+num_veins_slider.on_changed(update)
+main_vein_slider.on_changed(update)
 
+update(None)
 plt.show()
